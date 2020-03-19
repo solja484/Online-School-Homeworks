a20a48869e5a7fefaea85db47d4faa076b58ca3d
Соломія Андрусів <solomiia.andrusiv@ukma.edu.ua>
2020-03-19 14:51:02 +0200

    finish validation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let usertypes = ["admin", "teacher", "pupil"];
-let usertype = 0;
-
+// user-types = ["admin", "teacher", "pupil"];
+let usertype = 2;
+let registry=false;
 function change_entry_type(t) {
     if (t === 0) {
         $('#entryform').css("background", "#fd7172");
         usertype = 0;
     }
     else if (t === 1) {
-        $('#entryform').css("background", "#fee96a");
+        $('#entryform').css("background", "#fee96a"); //teacher
         usertype = 1;
     }
     else if (t === 2) {
@@ -17,14 +17,21 @@ function change_entry_type(t) {
 }
 
 function change_reg_type(t) {
-
     if (t === 1) {
         $('#registryform').css("background", "#fee96a");
         usertype = 1;
+        $('#pupil_form').hide();
+        $('#teacher_form').show();
+        if(registry)
+        checkValidity();
     }
     else {
         $('#registryform').css("background", "#7cdeeb");
         usertype = 2;
+        $('#pupil_form').show();
+        $('#teacher_form').hide();
+        if(registry)
+        checkValidity();
     }
 }
 
@@ -36,6 +43,12 @@ function shoW(except) {
 }
 
 
+/*
+$("#entrypanel").show();
+$("#registrypanel").hide();
+$("#admin_page").hide();
+$("#admin_page_tab").hide();
+$("#container").hide();*/
 let authentication = false;
 $("#entrypanel").show();
 $("#registrypanel").hide();
@@ -44,84 +57,120 @@ $("#admin_page_tab").hide();
 $("#container").hide();
 
 
-function register() {
-    console.log("try");
-    var pass = $('#reg_password').val();
-    var pass2 = $('#reg_password2').val();
-    if (!validName("reg_lastname") || !validName("reg_firstname") ||
-        !validPass("reg_password") || !validEmail("reg_email") || !validCode("reg_code")) {
-        return;
+function checkValidity(){
+    let res=[];
+    res.push(validName("reg_lastname"));
+    res.push(validName("reg_firstname"));
+    res.push(validFName("reg_fathername"));
+
+    res.push(validEmail("reg_email"));
+    res.push(validPass("reg_password"));
+    res.push(validCode("reg_code"));
+    res.push(validPhone("reg_phone"));
+    if(usertype==1){
+        res.push(validDocument("reg_teacher_code"));
+        res.push(validEmpty("reg_education"));
+    }else if(usertype==2) {
+        res.push(validClass("reg_class"));
+        res.push(validDocument("reg_student_code"));
+        res.push(validEmpty("reg_birth_date"));
     }
-    if (pass2 != pass) {
-        $('#reg_password2').css("background-color", "rgba(253, 113, 114, 0.5)");
-        return;
-    }
-    var data = {
-        "name": $("#reg_firstname").val(),
-        "surname": $("#reg_lastname").val(),
-        "patronymic": $("#reg_fathername").val(),
-        "email": $("#reg_email").val(),
-        "password": $("#reg_password").val(),
-        "phone": $("#reg_phone").val(),
-        "birth_date": $("#reg_birth_date").val(),
-        "class": $("#reg_class").val(),
-        "school_id": $("#reg_code").val()
-    };
-    $.ajax({
-        url: 'http://localhost:2303/registerpupil',
-        type: 'post',
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (data, status) {
-            alert(data.error);
-            authentication = true;
-            $("#registrypanel").hide();
-            $("#admin_page").hide();
-            $("#admin_page_tab").hide();
-            $("#entrypanel").hide();
-            $("#container").show();
-        },
-        data: JSON.stringify(data)
-    });
+    let success=true;
+    for(a of res)
+        if (!a) success = false;
+       return success;
 
 }
 
-function login() {
-    var login = $("#entry_email").val();
-    var pass = $("#entry_password").val();
-    if(pass=="" || login==""){
-        alert("Поля не можуть бути пустими");
-        return;
+function register(){
+    registry=true;
+    let valid = checkValidity();
+    let pass = $('#reg_password').val();
+    let pass2 = $('#reg_password2').val();
+    if (pass2 !== pass) {
+        document.getElementById('reg_password2').classList.remove('is-valid');
+        document.getElementById('reg_password2').classList.add('is-invalid');
+        valid=false;
     }
-    var data = {
-        "login": login,
-        "password": pass
-    };
-    $.ajax({
-        url: 'http://localhost:2303/registerpupil',
-        type: 'post',
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (data, status) {
-            authentication = true;
-            $("#entrypanel").hide();
-            $("#registrypanel").hide();
-            $("#container").show();
-            if (usertype == 0) {
-                $("#admin_page").show();
-                $("#admin_page_tab").show();
-            } else {
+
+    if(valid){
+
+        var data ={
+            "name": $("#reg_firstname").val(),
+            "surname": $("#reg_lastname").val(),
+            "patronymic": $("#reg_fathername").val(),
+            "email": $("#reg_email").val(),
+            "password": $("#reg_password").val(),
+            "phone": $("#reg_phone").val(),
+            "school_id": $("#reg_code").val()
+        };
+        if(usertype==1) {
+            data.education = $("#reg_education").val();
+            data.phd = $("#reg_phd").val();
+            data.teacher_id = $("#reg_teacher_code").val();
+        }else if(usertype==2){
+
+            data.birth_date= $("#reg_birth_date").val(),
+                data.class =$("#reg_class").val(),
+                data.student_id=$("#reg_student_code").val()
+        };
+        $.ajax({
+            url: 'http://localhost:2303/registerpupil',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data, status) {
+                alert(data.error);
+                authentication = true;
+                $("#registrypanel").hide();
                 $("#admin_page").hide();
                 $("#admin_page_tab").hide();
-            }
-            $("#container").show();
-        },
-        error: function(data, status){
-            alert("Переконайтесь в правильності введених даних");
-        },
-        data: JSON.stringify(data)
-    });
+                $("#entrypanel").hide();
+                $("#container").show();
+            },
+            data: JSON.stringify(data)
+        });
+        gotologin();
+    }
+}
 
+
+function login() {
+    let login = validEmpty('entry_email');
+    let pass=validEmpty('entry_password');
+    //var login = $("#entry_email").val();
+    //var pass = $("#entry_password").val();
+    if(pass && login) {
+
+        var data = {
+            "login": $("#entry_email").val(),
+            "password": $("#entry_password").val()
+        };
+        $.ajax({
+            url: 'http://localhost:2303/registerpupil',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data, status) {
+                authentication = true;
+                $("#entrypanel").hide();
+                $("#registrypanel").hide();
+                $("#container").show();
+                if (usertype == 0) {
+                    $("#admin_page").show();
+                    $("#admin_page_tab").show();
+                } else {
+                    $("#admin_page").hide();
+                    $("#admin_page_tab").hide();
+                }
+                $("#container").show();
+            },
+            error: function (data, status) {
+                alert("Переконайтесь в правильності введених даних");
+            },
+            data: JSON.stringify(data)
+        });
+    }
 }
 
 function exit() {
--- js/validation.js
+++ js/validation.js
@@ -1,49 +1,149 @@
 function validEmail(str) {
-    var name = $("#" + str).val();
+    let name = $("#" + str).val();
     if (name.length < 1 || name.length > 100) {
-        $("#" + str).css("background-color", "rgba(253, 113, 114, 0.5)");
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+
         return false;
     }
-    var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (name.match(pattern)) {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
         return true;
     } else {
-        $("#" + str).css("background", "rgba(253, 113, 114, 0.5)");
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+
         return false;
     }
 }
 
 function validName(str) {
-    var name = $("#" + str).val();
+    let name = $("#" + str).val();
     if (name.length < 1 || name.length > 45) {
-        $("#" + str).css("background-color", "rgba(253, 113, 114, 0.5)");
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+
         return false;
     }
-    var letters = /^[A-Za-zА-Яа-яі]+$/;
+    let letters = /^[A-Za-zА-Яа-яі]+$/;
     if (name.match(letters)) {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
         return true;
     } else {
-        $("#" + str).css("background", "rgba(253, 113, 114, 0.5)");
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+
         return false;
     }
+}
+
+function validFName(str) {
+    let name = $("#" + str).val();
+    if (name === "") {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
+        return true;
+    }
+    return validName(str);
+
 }
 
 //at least 6 characters eng letter number and symbol
 function validPass(str) {
-    var pass = $("#" + str).val();
-    var pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/;
+    let pass = $("#" + str).val();
+    let pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/;
     if (!pattern.test(pass)) {
-        $("#" + str).css("background-color", "rgba(253, 113, 114, 0.5)");
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+
         return false;
     }
+    document.getElementById(str).classList.remove('is-invalid');
+    document.getElementById(str).classList.add('is-valid');
     return true;
 }
 
 function validCode(str) {
-    var code = $("#" + str).val();
-    if (code.length==10){
+    let code = $("#" + str).val();
+    if (code.length == 10) {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
         return true;
     }
-    $("#" + str).css("background-color", "rgba(253, 113, 114, 0.5)");
+
+    document.getElementById(str).classList.remove('is-valid');
+    document.getElementById(str).classList.add('is-invalid');
     return false
+}
+
+function validClass(str) {
+    let klass = $("#" + str).val();
+    let isNum = /^\d+$/.test(klass);
+    let value = -1;
+    if (isNum) value = parseInt(klass);
+    if (isNum && value > 0 && value < 12) {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
+        return true;
+    }
+
+    document.getElementById(str).classList.remove('is-valid');
+    document.getElementById(str).classList.add('is-invalid');
+    return false;
+
+}
+
+function validDocument(str) {
+    let val = $("#" + str).val();
+    let pattern = /^\d+$/;//^[0-9]*$
+    if (val.length == 8 && val.match(pattern)) {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
+        return true;
+    }
+    document.getElementById(str).classList.remove('is-valid');
+    document.getElementById(str).classList.add('is-invalid');
+    return false;
+}
+
+function validPhone(str) {
+    let phone = $("#" + str).val();
+    let pattern=/^\d+$/;
+    let pattern2=/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+    if (phone === ""){
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
+        return true;
+
+    }else if(phone.match(pattern)&&phone.length===10) {
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
+        return true
+    }else if(phone.match(pattern2)){
+        document.getElementById(str).classList.remove('is-invalid');
+        document.getElementById(str).classList.add('is-valid');
+        return true
+    }
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+
+        return false;
+
+}
+
+
+
+function validEmpty(str) {
+    let edu = $("#" + str).val();
+    if (edu === "") {
+        document.getElementById(str).classList.remove('is-valid');
+        document.getElementById(str).classList.add('is-invalid');
+        return false;
+    }
+    document.getElementById(str).classList.remove('is-invalid');
+    document.getElementById(str).classList.add('is-valid');
+    return true;
 }
