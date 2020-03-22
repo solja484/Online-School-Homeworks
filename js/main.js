e2324403be3f43978b0717dc351a69548308e82e
n.shkarow@gmail.com <n.shkarow@gmail.com>
2020-03-22 17:34:02 +0200

    addAdmin work

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ $("#entrypanel").show();
 // $("#admin_page_tab").hide();
 // $("#subject_page").hide();
 // $("#container").show();
-
-shoW('subject_list_page');
+// shoW('subject_list_page');
 
 function change_entry_type(t) {
     if (t === 0) {
@@ -56,24 +55,6 @@ function show_subject() {
     shoW('subject_list_page');
     $("#subject_list_page").hide();
     $("#subject_page").show();
-}
-
-function checkValidity() {
-    if (!validName("reg_lastname") || !validName("reg_firstname") || !validFName("reg_fathername") ||
-        !validEmail("reg_email") || !validPass("reg_password") || !validCode("reg_code") || !validPhone("reg_phone")){
-        return false
-    }
-    var cur_user_type = localStorage.getItem("usertype");
-    if (cur_user_type === 'teacher') {
-        if(!validDocument("reg_teacher_code") || !validEmpty("reg_education")){
-            return false;
-        }
-    } else if (cur_user_type === 'pupil') {
-        if(!validClass("reg_class") || !validDocument("reg_student_code") || !validEmpty("reg_birth_date")){
-            return false;
-        }
-    }
-    return true;
 }
 
 function register() {
@@ -139,26 +120,64 @@ function login() {
         contentType: 'application/json',
         'accept': "application/json",
         success: function (data) {
-            localStorage.setItem('authentication', cur_user_type);
+            localStorage.setItem('authentication', data.id);
             $("#entrypanel").hide();
             $("#registrypanel").hide();
             if (cur_user_type === 'admin') {
                 $("#admin_page").show();
                 $("#admin_page_tab").show();
             }
-            // else {
-            //     $("#admin_page").hide();
-            //     $("#admin_page_tab").hide();
-            // }
             $("#container").show();
         },
-        error: function (data) {
+        error: function () {
             alert("Переконайтесь в правильності введених даних");
         },
         data: JSON.stringify(data)
     });
 }
-
+function addAdmin() {
+    var logSelector = $("#input_admin_login");
+    var login = logSelector.val();
+    if(login===""){
+        logSelector.addClass('is-invalid');
+        return false;
+    }
+    if(!validEmail("input_admin_email") || !validPass("input_admin_password") ||
+        !validName("input_admin_fname") || !validName("input_admin_lname")){
+        return false;
+    }
+    var data = {
+        "login": login,
+        "email": $("#input_admin_email").val(),
+        "password": $("#input_admin_password").val(),
+        "name": $("#input_admin_fname").val(),
+        "surname": $("#input_admin_lname").val(),
+    };
+    var notes = $("#input_admin_notes").val();
+    if(notes!==""){
+        data['notes'] = notes;
+    }
+    $.ajax({
+        url: 'http://localhost:2303/registeradmin',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        "accept": 'application/json',
+        success: function () {
+            $("#content").prepend("<div class='alert alert-success alert-dismissible'>" +
+                "<button type='button' class='close' data-dismiss='alert'>&times;</button>" +
+                "  <strong>Success!</strong> Новий адміністратор створений" +
+                "</div>");
+        },
+        error: function () {
+            $("#content").prepend("<div class='alert alert-danger alert-dismissible'>" +
+                "<button type='button' class='close' data-dismiss='alert'>&times;</button>" +
+                "  <strong>Error!</strong> Не вдалося додати адміністратора" +
+                "</div>");
+        },
+        data: JSON.stringify(data)
+    });
+}
 function exit() {
     localStorage.setItem("usertype","pupil");
     localStorage.removeItem("authentication");
--- js/validation.js
+++ js/validation.js
@@ -1,108 +1,126 @@
+
+function checkValidity() {
+    if (!validName("reg_lastname") || !validName("reg_firstname") || !validFName("reg_fathername") ||
+        !validEmail("reg_email") || !validPass("reg_password") || !validCode("reg_code") || !validPhone("reg_phone")){
+        return false
+    }
+    var cur_user_type = localStorage.getItem("usertype");
+    if (cur_user_type === 'teacher') {
+        if(!validDocument("reg_teacher_code") || !validEmpty("reg_education")){
+            return false;
+        }
+    } else if (cur_user_type === 'pupil') {
+        if(!validClass("reg_class") || !validDocument("reg_student_code") || !validEmpty("reg_birth_date")){
+            return false;
+        }
+    }
+    return true;
+}
+
+
 function validEmail(str) {
-    var selctor = $("#" + str);
+    var selector = $("#" + str);
     let name = selector.val();
     if (name.length < 1 || name.length > 100) {
-        selctor.removeClass('is-valid');
-        selctor.addClass('is-invalid');
+        selector.removeClass('is-valid');
+        selector.addClass('is-invalid');
         return false;
     }
     let pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (name.match(pattern)) {
-        selctor.removeClass('is-invalid');
-        selctor.addClass('is-valid');
+        selector.removeClass('is-invalid');
+        selector.addClass('is-valid');
         return true;
     } else {
-        selctor.removeClass('is-valid');
-        selctor.addClass('is-invalid');
+        selector.removeClass('is-valid');
+        selector.addClass('is-invalid');
 
         return false;
     }
 }
 
 function validName(str) {
-    var selctor = $("#" + str);
+    var selector = $("#" + str);
     let name = selector.val();
     if (name.length < 1 || name.length > 45) {
-        selctor.removeClass('is-valid');
-        selctor.addClass('is-invalid');
+        selector.removeClass('is-valid');
+        selector.addClass('is-invalid');
         return false;
     }
     let letters = /^[A-Za-zА-Яа-яі]+$/;
     if (name.match(letters)) {
-        selctor.removeClass('is-invalid');
-        selctor.addClass('is-valid');
+        selector.removeClass('is-invalid');
+        selector.addClass('is-valid');
         return true;
     } else {
-        selctor.removeClass('is-valid');
-        selctor.addClass('is-invalid');
+        selector.removeClass('is-valid');
+        selector.addClass('is-invalid');
         return false;
     }
 }
 
 function validFName(str) {
-    var selctor = $("#" + str);
+    var selector = $("#" + str);
     let name = selector.val();
     if (name === "") {
-        selctor.removeClass('is-invalid');
-        selctor.addClass('is-valid');
+        selector.removeClass('is-invalid');
+        selector.addClass('is-valid');
         return true;
     }
     return validName(str);
-
 }
 
 //at least 6 characters eng letter number and symbol
 function validPass(str) {
-    var selctor = $("#" + str);
+    var selector = $("#" + str);
     let pass = selector.val();
     let pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/;
     if (!pattern.test(pass)) {
         selector.removeClass('is-valid');
-        selctor.addClass('is-invalid');
-
+        selector.addClass('is-invalid');
         return false;
     }
     selector.removeClass('is-invalid');
-    selctor.addClass('is-valid');
+    selector.addClass('is-valid');
     return true;
 }
 
 function validCode(str) {
-    var selctor = $("#" + str);
+    var selector = $("#" + str);
     let code = selector.val();
     if (code.length === 10) {
         selector.removeClass('is-invalid');
-        selctor.addClass('is-valid');
+        selector.addClass('is-valid');
         return true;
     }
     selector.removeClass('is-valid');
-    selctor.addClass('is-invalid');
+    selector.addClass('is-invalid');
     return false
 }
 
 function validClass(str) {
-    var selctor = $("#" + str);
-    let klass = selctor.val();
+    var selector = $("#" + str);
+    let klass = selector.val();
     let isNum = /^\d+$/.test(klass);
     let value = -1;
     if (isNum) value = parseInt(klass);
     if (isNum && value > 0 && value < 12) {
-        selctor.removeClass('is-invalid');
-        selctor.addClass('is-valid');
+        selector.removeClass('is-invalid');
+        selector.addClass('is-valid');
         return true;
     }
     selector.removeClass('is-valid');
-    selctor.addClass('is-invalid');
+    selector.addClass('is-invalid');
     return false;
 
 }
 
 function validFreeClass(str) {
-    var selctor = $("#" + str);
-    let val = selctor.val();
+    var selector = $("#" + str);
+    let val = selector.val();
     if (val === "") {
-        selctor.removeClass('is-invalid');
-        selctor.addClass('is-valid');
+        selector.removeClass('is-invalid');
+        selector.addClass('is-valid');
         return true;
     }
     return validClass(str);
@@ -141,11 +159,11 @@ function validPhone(str) {
 }
 
 function validEmpty(str) {
-    var selctor = $("#" + str);
+    var selector = $("#" + str);
     let edu = selector.val();
     if (edu === "") {
         selector.removeClass('is-valid');
-        selctor.addClass('is-invalid');
+        selector.addClass('is-invalid');
         return false;
     }
     return true;
